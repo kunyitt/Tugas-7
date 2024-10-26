9f505909306fa98a95fa9056e1ffcd96b161c4f4
kunyitt <trimlyni15@gmail.com>
2024-10-26 13:34:42 +0700

    implementasi fungsi

--- index.js
+++ index.js
@@ -8,10 +8,19 @@ switch (command) {
     case "make-folder":
         app.makeFolder()
         break;
-
+    case "make-file":
+        app.makeFile()
+        break;
+    case "ext-sorter":
+        app.extSorter()
+        break;
+    case "read-folder":
+        app.readFolder()
+        break;
+    case "read-file":
+        app.readFile()
+        break;
     default:
         throw Error("Invalid command")
         break;
 }
- 
-
